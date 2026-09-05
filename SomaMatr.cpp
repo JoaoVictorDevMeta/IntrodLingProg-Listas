67fcaff0e07b04c9eb97c735407e76af8463f75f
JotaDevVictor <joaovictorgouveiar@gmail.com>
2026-09-05 17:10:44 -0300

    pequena correção na logica da SomaMztr

--- SomaMatr.cpp
+++ SomaMatr.cpp
@@ -2,12 +2,25 @@
 
 using namespace std;
 
+void SomaExibMat(int m, int n, vector<vector<int>> mat1, vector<vector<int>> mat2){
+
+    for(int i = 0; i < m; i++){
+        for(int j =0;j < n ; j++){
+            cout << mat1[i][j]+mat2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return;
+}
+
 int main(){
-    int m,n, alt;
+    int m,n;
 
     cin >> m >> n;
 
-    int matz[m][n];
+    vector<vector<int>> matz(m, vector<int>(n));
+    vector<vector<int>> matzsecond(m, vector<int>(n));
 
     //cout << m << " " << n << endl;
 
@@ -19,18 +32,11 @@ int main(){
 
     for(int i = 0; i < m; i++){
         for(int j =0;j < n ; j++){
-            alt = matz[i][j];
-            cin >> matz[i][j];
-            matz[i][j] += alt;
+            cin >> matzsecond[i][j];
         }
     }
 
-    for(int i = 0; i < m; i++){
-        for(int j =0;j < n ; j++){
-            cout << matz[i][j] << " ";
-        }
-        cout << endl;
-    }
+    SomaExibMat(m , n, matz, matzsecond);
 
     return 0;
 }
